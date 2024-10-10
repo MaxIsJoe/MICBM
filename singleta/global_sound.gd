a6f2f9513e381d030f2413dd3e184cb8ae5bb4e5
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2024-10-10 05:18:49 +0300

    fix volume settings being incorrectly set

--- singleta/global_sound.gd
+++ singleta/global_sound.gd
@@ -5,7 +5,6 @@ var sfx: Dictionary = {
 	"coin": [preload("res://audio/sfx/coins0_0.wav"), preload("res://audio/sfx/coins0_1.wav"), preload("res://audio/sfx/coins0_2.wav"), preload("res://audio/sfx/coins0_3.wav"), ]
 }
 
-var current_music: MusicPlayer = null
 var bus_layout: AudioBusLayout = preload("res://audio/audio_bus.tres")
 var gameplay_mixer: int
 
@@ -21,32 +20,19 @@ func play_sfx_2d(what: String, where: Vector2) -> SFX2D:
 	Game.deploy_instance(new_sfx, where)
 	return new_sfx
 
-func play_music(what: AudioStream):
-	var new_music = MusicPlayer.new()
-	new_music.stream = what
-	if is_instance_valid(current_music):
-		current_music.queue_free()
-	current_music = new_music
-	add_child(new_music)
-
-func stop_music():
-	if is_instance_valid(current_music):
-		current_music.stop()
-
 func pitch_shift(shift_to: float, time: float):
 	var tween: Tween = get_tree().create_tween()
 	tween.tween_property(AudioServer.get_bus_effect(gameplay_mixer, 0), "pitch_scale", shift_to, time)
 	
 func lower_bus_volume(bus_index: int, shift_to: float, time: float):
 	var tween: Tween = get_tree().create_tween()
-	tween.tween_method(change_audio_bus_volume_arrayArg, 
-		[gameplay_mixer, AudioServer.get_bus_volume_db(gameplay_mixer)], 
-		[gameplay_mixer, shift_to], 
+	tween.tween_method(change_audio_bus_volume_arrayArg,
+		[gameplay_mixer, AudioServer.get_bus_volume_db(gameplay_mixer)],
+		[gameplay_mixer, shift_to],
 		time)
 	
-func change_audio_bus_volume(bus_index:int, value: float):
+func change_audio_bus_volume(bus_index: int, value: float):
 	AudioServer.set_bus_volume_db(bus_index, value)
 
 func change_audio_bus_volume_arrayArg(args: Array):
 	AudioServer.set_bus_volume_db(args[0], args[1])
-	print(AudioServer.get_bus_volume_db(gameplay_mixer))
--- ui/optionsscreen.gd
+++ ui/optionsscreen.gd
@@ -7,22 +7,23 @@ func _ready() -> void:
 
 
 func set_element_values():
-	%master_slider.value = db_to_linear( OptionsManager.options["master_volume"] )
-	%music_slider.value = db_to_linear( OptionsManager.options["music_volume"] )
-	%sfx_slider.value = db_to_linear( OptionsManager.options["sound_volume"] )
+	OptionsManager._load_options()
+	%master_slider.value = OptionsManager.options["master_volume"]
+	%music_slider.value = OptionsManager.options["music_volume"]
+	%sfx_slider.value = OptionsManager.options["sound_volume"]
 	%fullscreen_button.button_pressed = (DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN or DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
 
 
 func _on_master_slider_value_changed(value: float) -> void:
-	AudioServer.set_bus_volume_db( 0, linear_to_db(value) )
+	AudioServer.set_bus_volume_db(0, linear_to_db(value))
 	OptionsManager.set_master_volume_setting(value)
 
 func _on_music_slider_value_changed(value: float) -> void:
-	AudioServer.set_bus_volume_db( 1, linear_to_db(value) )
+	AudioServer.set_bus_volume_db(1, linear_to_db(value))
 	OptionsManager.set_music_volume_setting(value)
 
 func _on_sfx_slider_value_changed(value: float) -> void:
-	AudioServer.set_bus_volume_db( 2, linear_to_db(value) )
+	AudioServer.set_bus_volume_db(2, linear_to_db(value))
 	OptionsManager.set_sound_volume_setting(value)
 
 func _on_fullscreen_button_toggled(toggled_on: bool) -> void:
