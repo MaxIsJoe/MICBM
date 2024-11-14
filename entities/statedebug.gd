0d12f8da655c7d3b145c34aaca0be42676532d7d
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2024-11-14 13:33:29 +0200

    add velocity to state info

--- entities/statedebug.gd
+++ entities/statedebug.gd
@@ -2,4 +2,5 @@ extends Label
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	text = $"../fsm".state_name
+	var velspeed = get_parent().velocity
+	text = $"../fsm".state_name + "\n" +str(velspeed)
