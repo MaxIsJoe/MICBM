e714e0d4ae9c42d1baffa3635555f30386d0c95a
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2024-12-05 03:15:36 +0200

    fix wrong bus settings

--- ui/optionsscreen.gd
+++ ui/optionsscreen.gd
@@ -19,11 +19,11 @@ func _on_master_slider_value_changed(value: float) -> void:
 	OptionsManager.set_master_volume_setting(value)
 
 func _on_music_slider_value_changed(value: float) -> void:
-	AudioServer.set_bus_volume_db(1, linear_to_db(value))
+	AudioServer.set_bus_volume_db(2, linear_to_db(value))
 	OptionsManager.set_music_volume_setting(value)
 
 func _on_sfx_slider_value_changed(value: float) -> void:
-	AudioServer.set_bus_volume_db(2, linear_to_db(value))
+	AudioServer.set_bus_volume_db(3, linear_to_db(value))
 	OptionsManager.set_sound_volume_setting(value)
 
 func _on_fullscreen_button_toggled(toggled_on: bool) -> void:
